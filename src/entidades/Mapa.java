4c5149bab3c7047a8d52ccfd35c1fa088e33b201
Adri4Code <afdezramos@gmail.com>
2017-01-09 11:33:57 +0100

    Punto 4 de la EC2 (Personajes ) (4)

--- src/entidades/Mapa.java
+++ src/entidades/Mapa.java
@@ -1,6 +1,9 @@
 package entidades;
 
+import Personajes.Caminante;
+import Personajes.Lannister;
 import Personajes.Stark;
+import Personajes.Targaryen;
 
 /**
  * Created by adri on 18/12/16.
@@ -12,6 +15,7 @@ public class Mapa {
     private Sala[][] matrizSalas = new Sala[6][6];
     Llave[] reparto = new Llave[45];
     private Sala salaGanadores = new Sala(1111);
+    private int turno = 0;
 
     public Mapa() {
         this.Dim1 = 0;
@@ -85,6 +89,19 @@ public class Mapa {
         this.salaGanadores = ganadores;
     }
 
+    public boolean haySala(int id) {
+        boolean existe = false;
+        for (int i = 0; i < this.getDim1(); i++) {
+            for (int j = 0; j < this.getDim2(); j++) {
+                if (id == this.matrizSalas[i][j].getId()) {
+                    existe = true;
+                } else {
+                    existe = false;
+                }
+            }
+        }
+        return existe;
+    }
     public void setReparto() {
 
         int i = 0;
@@ -149,18 +166,43 @@ public class Mapa {
 
     }
 
+    public void incrementarTurno() {
+        this.turno++;
+    }
+
+    public void insertarPersonajes() {
+        Stark Tony = new Stark("Ironman", 'T');
+        Targaryen Clarke = new Targaryen("Emilie ", 'E');
+        Caminante Bicho = new Caminante("Cr7", 'C');
+        Lannister Blake = new Lannister("Griffin", 'B');
+        this.matrizSalas[0][0].insertarPersonajesSala(Tony);
+        this.matrizSalas[0][0].insertarPersonajesSala(Clarke);
+        this.matrizSalas[0][5].insertarPersonajesSala(Bicho);
+        this.matrizSalas[5][5].insertarPersonajesSala(Blake);
+
+    }
+
+    public void Simulacion() {
+        insertarPersonajes();
+        //llega a su fin cuando se ha conseguido abrir la puerta o si se alcanzan 50 turnos sin que se abra
+
+
+    }
+
     public enum Dir {S, E, N, O}
 
-    ;
 
     public static void main(String[] args) {
-        Mapa m = new Mapa(6, 6);
+       /* Mapa m = new Mapa(6, 6);
         m.setMatrizSalas();
         m.setReparto();
         m.repartirLlaves();
         Stark Tony = new Stark("Iron man ", 'T');
         Dir[] direccionesStark = new Dir[]{Dir.S,};
-        Tony.movimientoPersonaje(direccionesStark);
+        int v = 0;
+        int k = 5;
+        Tony.movimientoPersonaje(direccionesStark, m.matrizSalas, v, k);
+*/
 
     }
 }
